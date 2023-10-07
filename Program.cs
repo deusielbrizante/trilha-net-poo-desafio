6d09414dc6106b57fff8477584cceb133afee1b5
Deusiel <deusielbrizante001@gmail.com>
2023-10-07 12:40:21 -0300

    Desafio DIO completado com sucessogit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia(numero: "92836", modelo: "Tijolão", imei: "162754712", memoria: 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Genshin Impact");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(numero: "31231", modelo: "Mais Caro", imei: "621867451", memoria: 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Clash Royale");
